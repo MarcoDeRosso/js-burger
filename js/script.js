86d2377a7e376f0c573f77d6f12ca436ebdfbad7
MarcoDeRosso <mcoder@hotmail.it>
2021-06-07 16:29:06 +0200

    discount ok

--- js/script.js
+++ js/script.js
@@ -2,18 +2,21 @@ var createSum = document.getElementById("somma-button");
 
 var resultSum = document.getElementById("price")
 
-var discount = 20 / 100;
+
 
 createSum.addEventListener("click", function() {
     var resultSumInt = 0;
     var checkNum = document.getElementsByClassName("check-ingredients");
-    var checkDiscount = document.getElementsByClassName("discount-code")[0];
+    var checkDiscount = document.getElementById("discount");
     for (var i = 0; i < checkNum.length; i++) {
         if (checkNum[i].checked) {
             resultSumInt += parseInt(checkNum[i].value);
-        } else if ((checkNum[i].checked) && (checkDiscount[i] === "McBoolean")) {
-            resultSumInt += parseInt(checkNum[i].value) - (parseInt(checkNum[i].value) * discount);
         }
     }
+    if (checkDiscount.value === "McBoolean") {
+        var sconto = resultSumInt * 20 / 100;
+        resultSum.innerHTML = resultSumInt - sconto + "€";
+    }
     resultSum.innerHTML = resultSumInt + "€";
+
 })
